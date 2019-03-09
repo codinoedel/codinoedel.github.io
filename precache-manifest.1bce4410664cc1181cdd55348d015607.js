31972548d2ad5c8f93e4f5cc4343b707ae409e86
Codi Noedel <codi.noedel@gmail.com>
2019-03-09 14:14:01 -0800

    Updates

---
+++ precache-manifest.1bce4410664cc1181cdd55348d015607.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7e911ee72d2265447f921063addc7b1",
+    "url": "/static/media/codi.a7e911ee.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "518489e064a0d9930efb",
+    "url": "/static/js/main.518489e0.chunk.js"
+  },
+  {
+    "revision": "89622b928a8a01cac242",
+    "url": "/static/js/2.89622b92.chunk.js"
+  },
+  {
+    "revision": "518489e064a0d9930efb",
+    "url": "/static/css/main.cbc5a605.chunk.css"
+  },
+  {
+    "revision": "22ae055f75725a0b066b73d754f593ce",
+    "url": "/index.html"
+  }
+];
