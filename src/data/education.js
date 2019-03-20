7ffff848b7f4dc371276cae3221d338b7f12eb40
Codi Noedel <codi.noedel@gmail.com>
2019-03-19 20:00:52 -0700

    Add more information on college coursework

--- src/data/education.js
+++ src/data/education.js
@@ -5,7 +5,7 @@ export const education = [
     completionDate: '2013-06-12T00:00:00Z',
     achievements:  [
       { value: 'Founding member of the Evergreen Collegiate Cyber Defense Competition Blue Team (2012, 2013)' },
-      { value: 'Implemented a simple C interpreter in Java as part of a program exploring language design & theory' },
+      { value: 'Focus on data structures, operating systems, and language design & theory, including implementation of a basic C interpreter in Java' },
     ]
   }
 ];
