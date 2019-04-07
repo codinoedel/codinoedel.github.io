c9843332ae24f3aad6500ba06ec8b9cb6ab79e44
Codi Noedel <codi.noedel@gmail.com>
2019-04-07 13:22:26 -0700

    1st person to 3rd person

--- src/data/basicinfo.js
+++ src/data/basicinfo.js
@@ -5,7 +5,7 @@ export const basicinfo = {
   location: 'Edmonds, WA',
   name:     'Codi Noedel',
   title:    'Senior Full-Stack Software Engineer',
-  bio:  "Codi is a Software Engineer currently working on the Unified Demonstration Framework team at F5 Networks, where she builds tools and automation frameworks that allow F5 to more effectively distribute its technical content. She's passionate about designing and building software that delights and empowers its users. As a full-stack developer, she feels at home building both complex systems and robust, user-focused web clients.\n\nIn her free time, she plays video games, reads a lot of books, studies languages, builds web apps, and spends time with her friends and family. ",
+  bio:  "I am a Software Engineer currently working on the Unified Demonstration Framework team at F5 Networks, where I build tools and automation frameworks that allow F5 to more effectively distribute its technical content. I'm passionate about designing and building software that delights and empowers its users. As a full-stack developer, I feels at home building both complex systems and robust, user-focused web clients.\n\nIn my free time, I play video games, read a lot of books, study languages, build web apps, and spend time with my friends and family. ",
 };
 
 export default basicinfo;
