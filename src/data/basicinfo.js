56950aa17101ee3152ae89348eba5efcb292079a
Codi Noedel <c.noedel@f5.com>
2019-09-16 10:08:14 -0700

    Update hobbies

--- src/data/basicinfo.js
+++ src/data/basicinfo.js
@@ -5,7 +5,7 @@ export const basicinfo = {
   location: 'Edmonds, WA',
   name:     'Codi Noedel',
   title:    'Senior Full-Stack Software Engineer',
-  bio:  "I am a Software Engineer currently working on the Unified Demonstration Framework team at F5 Networks, where I build tools and automation frameworks that allow F5 to more effectively distribute its technical content. I'm passionate about designing and building software that delights and empowers its users. As a full-stack developer, I feels at home building both complex systems and robust, user-focused web clients.\n\nIn my free time, I play video games, read a lot of books, study languages, build web apps, and spend time with my friends and family. ",
+  bio:  "I am a Software Engineer currently working on the Unified Demonstration Framework team at F5 Networks, where I build tools and automation frameworks that allow F5 to more effectively distribute its technical content. I'm passionate about designing and building software that delights and empowers its users. As a full-stack developer, I feels at home building both complex systems and robust, user-focused web clients.\n\nIn my free time, I play video games, study languages and tech history, and spend time with my friends and family. I'm currently learning 6502 assembly in the context of NES development.",
 };
 
 export default basicinfo;
